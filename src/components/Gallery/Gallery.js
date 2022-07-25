0cba9b8aa11f7c288b27716a6244a15b337c8c35
Jason Kobuchi <jekobuchi@gmail.com>
2022-07-25 15:15:33 -0700

    Adding button functionality for scrolling through gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -8,6 +8,7 @@ import './Gallery.css';
 const Gallery = () => {
 
     const [select,setSelect] = useState(0);
+
     const moveLeft = () => {
         if(select === 0) {
             setSelect(2)
@@ -22,17 +23,28 @@ const Gallery = () => {
             setSelect(select + 1)
         }
     }
+    const [content, setContent] = useState([
+        ["Manufactured with the best materials","We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."],
+        ["We are available all across the globe","With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we're in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."],
+        ["Discover innovative ways to decorate","Our modern furniture store provides a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."]
+    ])
 
 
     return(
         <div className='gallery-container'>
                 <div className='gallery-images'>
-                    <img src={galleryFirst}></img>
+                    {select === 0 
+                        ? <img src={galleryFirst} alt='gallery-first'></img>
+                        : select === 1 
+                            ? <img src={gallerySecond} alt='gallery-second'></img>
+                            : <img src={galleryThird} alt='gallery-third'></img>
+                    }
+                    
                 </div>
                 <div className='gallery-text'>
-                    <p></p>
-                    <p></p>
-                    <a><span>SHOP NOW</span></a>
+                    <p>{content[select][0]}</p>
+                    <p>{content[select][1]}</p>
+                    <a><span>SHOP NOW <img src={galleryArrow} alt='gallery-arrow'></img></span></a>
                     <div className='gallery-buttons'>
                         <button onClick={moveLeft}><span id='left-arrow'>&#60;</span></button>
                         <span>{select}</span>
