da6389562a368e1b237b259f2841dc3ee09327ab
Jason Kobuchi <jekobuchi@gmail.com>
2022-07-26 06:30:02 -0700

    Adding button functionality for scrolling through gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -24,9 +24,10 @@ const Gallery = () => {
         }
     }
     const [content, setContent] = useState([
-        ["Manufactured with the best materials","We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."],
+
+        ["Discover innovative ways to decorate","Our modern furniture store provides a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."],
         ["We are available all across the globe","With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we're in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."],
-        ["Discover innovative ways to decorate","Our modern furniture store provides a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."]
+        ["Manufactured with the best materials","We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."]
     ])
 
 
